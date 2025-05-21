bc043c9ca1e4ad8890e5854e530b321a1409f906
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-05-21 17:27:31 +0000

    The correct basic python operations

---
+++ basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+#Addition of numbers
+print("Addition of ", number1, "and", number2, "is", number1 + number2)
+
+#subtraction of numbers
+print("Subtraction of ", number1, "and ", number2, "is ", number1 - number2)
+
+#multiplication of numbers
+print("Multiplication of ", number1, "and ", number2, "is ", number1 * number2)
+
