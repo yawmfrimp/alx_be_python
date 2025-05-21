7544f41c2a4f4e95683b78be393ed948e514f51c
Yaw Mfum-Frimpong <63931469+yawmfrimp@users.noreply.github.com>
2025-05-21 23:57:04 +0000

    update to variables

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
 monthly_income = int(input("Enter your monthly income: "))
-monthly_exp = int(input('Enter your total monthly expenses: '))
+monthly_expenses = int(input('Enter your total monthly expenses: '))
 
 #calculating monthly savings
-monthly_savings = monthly_income - monthly_exp
+monthly_savings = monthly_income - monthly_expenses
 
 #calculating projected savings
 projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
