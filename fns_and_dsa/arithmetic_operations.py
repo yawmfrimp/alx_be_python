7fe8a340581c099d2c86cbb1e710137c5e82ddb8
Yaw Mfum-Frimpong <63931469+yawmfrimp@users.noreply.github.com>
2025-06-10 23:20:09 +0000

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,5 +7,6 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return (num1 * num2)
     elif operation == 'divide':
-        while num2 != 0:
-            return (num1 / num2)
+        if num2 == 0:
+            print(f'Error: cannot divide by zero')
+        return (num1 / num2)
