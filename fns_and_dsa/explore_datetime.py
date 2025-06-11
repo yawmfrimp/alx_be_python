fdf3caa08dad762f9d5072b5a0bac10ce8c6c597
Yaw Mfum-Frimpong <63931469+yawmfrimp@users.noreply.github.com>
2025-06-11 14:00:14 +0000

    script to determine future date
    determines future date by using current days + number of days

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,19 @@
+import datetime #import the module for datetime
+from datetime import timedelta #import modules for calculating a future date with amount of days 
+
+
+def display_current_datetime(): # defining function to display current datetime
+    current_date = datetime.datetime.now()
+    return current_date
+
+
+
+def calculate_future_date(): #defining function to calculate future date using timedelta
+    a = display_current_datetime() #initializing the variable of current time to a
+    b = a.strftime("%Y-%m-%d %H:%M:%S") #formating the currentime to display yyyy-mm-d H:M:S without the milliseconds
+    print(f'Current date and time: {b}') # print the current date
+    number_of_days = int(input("Enter a number of days to add to the current date: "))
+    future_date = a + timedelta(days=number_of_days)
+    print(f'Future date: {future_date.strftime("%Y-%m-%d")}') #print future date with appropriate formatting
+
+calculate_future_date()
