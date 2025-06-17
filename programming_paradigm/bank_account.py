f3ced6e8b4a91e2acab85e5199078088be425601
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-06-17 16:12:41 +0000

    yesss

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,7 +15,7 @@ class BankAccount:
 
 
     def display_balance(self):
-        print(f"Current balance: {self.account_balance}")
+        print("Current Balance:",self.account_balance)
 
 
         
