ee4a5bbed76202198b0dfff9c819fa72d625b587
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-06-17 16:38:31 +0000

    let this be it

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,7 +15,9 @@ class BankAccount:
 
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance: .2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
+        
+    
     
 
 
