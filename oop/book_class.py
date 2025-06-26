fe5dd8e7b1dd1b7efa7dcac8c3b78fba8106fe54
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-06-26 14:07:31 +0000

    removed apostrophe

--- oop/book_class.py
+++ oop/book_class.py
@@ -17,6 +17,6 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book('{self.title}','{self.author}', '{self.year}')"
+        return f"Book('{self.title}','{self.author}', {self.year})"
 
         
