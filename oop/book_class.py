eef6214e4881ee1dfa3fd27b35d4b5d6f2f81670
Yaw Mfum-Frimpong <yawmfrimp@yahoo.com>
2025-06-26 14:04:57 +0000

    oop assignment

---
+++ oop/book_class.py
@@ -0,0 +1,22 @@
+class Book:
+    def __init__(self,title,author,year):
+        if not isinstance(title,str):
+            raise TypeError('Title has to be a string')
+        if not isinstance(author, str):
+            raise TypeError('Author has to be a string')
+        if not isinstance(year, int):
+            raise TypeError('Year has to be a string')
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
+    def __repr__(self):
+        return f"Book('{self.title}','{self.author}', '{self.year}')"
+
+        
